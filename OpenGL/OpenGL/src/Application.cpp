89a0687b5e2a05143921f73e84e25b0c351722dc
SamBlesswin <samblesswinedu@gmail.com>
2022-04-25 12:25:31 +0530

    [Added]GLFW static library and Rendered a triangle in immediate mode

---
+++ OpenGL/OpenGL/src/Application.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    //Initialize GLFW
+    glfwInit();
+
+    /*State setting function of GLFW has GLFW_ prefix*/
+    
+    // OpenGL version 3.3 
+    //setting state context version to 3 and above
+    //This ensures that when a user does not have the proper OpenGL version GLFW fails to run. 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //setting opengl rendering profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+    //creating a window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GPU Rendering", NULL, NULL);
+
+    //Error check
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    //setting all the context for current window
+    glfwMakeContextCurrent(window);
+
+    //Rendering area
+    glViewport(0, 0, 800, 600);
+
+    //keep window open untill closed
+    while (!glfwWindowShouldClose(window))
+    {
+        //clearing the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Immediate mode 
+
+        glBegin(GL_TRIANGLES);
+            glColor3f(1, 0, 0);
+            glVertex2f(-0.5f, -0.5f);
+
+            glColor3f(0, 1, 0);
+            glVertex2f(0, 0.5f);
+
+            glColor3f(0, 0, 1);
+            glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /*When an application draws in a single buffer the resulting image may display flickering issues.*/
+        /*Double buffering : The front buffer contains the final output image that is shown at the screen, 
+        while all the rendering commands draw to the back buffer.*/
+        // Swap front and back buffers 
+        glfwSwapBuffers(window);
+
+        //function checks if any events are triggered (like keyboard input or mouse movement events)
+        glfwPollEvents();
+    }
+
+    //Terminate GLFW
+    glfwTerminate();
+    return 0;
+}
